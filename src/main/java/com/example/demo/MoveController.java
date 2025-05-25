d05ee8347cb4a4703af0b6ebaa983abf7f0e2ac2
Christian165293 <christian.bourquin05@gmail.com>
2025-05-25 17:55:34 -0500

    Finished the logic however tests are still needed

---
+++ src/main/java/com/example/demo/MoveController.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class MoveController {
+
+    @GetMapping("/computer")
+    public Space getTakenSpaces(@RequestParam(required = false) List<Integer> spaces) {
+        return determineMove(spaces);
+    }
+
+    public Space determineMove(List<Integer> inTakenSpaces) {
+        if (inTakenSpaces == null) {// /computer -> No board data provided
+            return new Space(null);
+        } else if (inTakenSpaces.isEmpty()) {// /computer?spaces -> Empty board - first move goes to space 1
+            return new Space(1);
+        } else if (inTakenSpaces.contains(1) && inTakenSpaces.size() == 1) {// /computer?spaces=1 -> Board with only space 1 marked - move to space 2
+            return new Space(2);
+        } else if (inTakenSpaces.containsAll(Arrays.asList(1, 3, 7))) {// /computer?spaces=1,3,7 -> Board with spaces 1, 3, and 7 marked - move to space 2
+            return new Space(2);
+        } else {//Default case
+            return new Space(444);
+        }
+    }
+
+}
---
+++ src/main/java/com/example/demo/Space.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+public class Space {
+    private Integer space;
+
+    public Space(){}
+
+    public Space(Integer space){
+        this.space = space;
+    }
+
+    public Integer getSpace(){
+        return space;
+    }
+    public void setSpace(Integer space){
+        this.space = space;
+    }
+}
